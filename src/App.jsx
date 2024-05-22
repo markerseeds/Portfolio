7e9efa2f87f677aa1f1be651418b238ce9e589b1
markerseeds <mark.see86@gmail.com>
2024-05-22 19:11:17 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+
+const App = () => {
+	return (
+		<React.Fragment>
+			<Navbar />
+			<Hero />
+		</React.Fragment>
+	);
+};
+
+export default App;
